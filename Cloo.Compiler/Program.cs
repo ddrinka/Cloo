7dfb6a62ede01cffc6232e3c245647206c62f4b6
JcBernack <jc.bernack@gmail.com>
2015-04-17 05:04:48 +0200

    Add build log parser for the Intel OpenCL compiler
    Add settings to choose a preferred platform and device type
    Add example messages from both the AMD and the Intel compiler

--- Cloo.Compiler/Program.cs
+++ Cloo.Compiler/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
+using Cloo.Compiler.Properties;
 
 namespace Cloo.Compiler
 {
@@ -16,31 +18,13 @@ namespace Cloo.Compiler
     /// </remarks>
     public class KernelCompiler
     {
-        // OpenCL example error messages:
-/*
-"C:\Users\xxx\AppData\Local\Temp\OCL916T8.cl", line 6: error: identifier "s"
-          is undefined
-    if (idx < num) x[idx] = x[idx] + y[idx];s
-  	                                        ^
+        private static readonly Regex ErrorRegexIntel = new Regex(@"^\d+:(\d+):(\d+): (\w+): (.*)$", RegexOptions.Multiline);
+        private static readonly Regex ErrorRegexAmd = new Regex(@"^""(.*)"", line (\d+): error: (.*)$", RegexOptions.Multiline);
+        private static readonly Regex TempFileRegexAmd = new Regex(@"^(.*OCL.*cl)$");
 
-"C:\Users\xxx\AppData\Local\Temp\OCL916T8.cl", line 7: error: expected a ";"
-  }
-  ^
+        private const string VendorIntel = "Intel(R) Corporation";
+        private const string VendorAmd = "Advanced Micro Devices, Inc";
 
-2 errors detected in the compilation of "C:\Users\xxx\AppData\Local\Temp\OCL916T8.cl".
-Frontend phase failed compilation.
-*/
-
-        /// <summary>
-        /// Matches essential parts of build log messages.
-        /// </summary>
-        private static readonly Regex ErrorRegex = new Regex(@"^""(.*)"", line (\d+): error: (.*)$", RegexOptions.Multiline);
-        
-        /// <summary>
-        /// Matches the random temporary filename assigned by the OpenCL driver.
-        /// </summary>
-        private static readonly Regex TempFileRegex = new Regex(@"^(.*OCL.*cl)$");
-        
         public static void Main(string[] args)
         {
             if (args.Length != 1)
@@ -50,11 +34,31 @@ Frontend phase failed compilation.
                 Console.WriteLine("Error messages are formatted so that Visual Studio can properly parse them into the error list.");
                 return;
             }
-            // initialize context and command queue for a GPU device
-            var platform = ComputePlatform.Platforms[0];
-            var context = new ComputeContext(ComputeDeviceTypes.Gpu, new ComputeContextPropertyList(platform), null, IntPtr.Zero);
+            // retrieve target platform in the following order:
+            // 1. match vendor name to the one given in the settings
+            // 2. try to get the intel plattform (because the compiler has nice output)
+            // 3. use the first platform available
+            var platform = ComputePlatform.Platforms.FirstOrDefault(_ => _.Vendor.Contains(Settings.Default.PlattformVendor)) ??
+                           ComputePlatform.Platforms.FirstOrDefault(_ => _.Vendor.Contains(VendorIntel)) ??
+                           ComputePlatform.Platforms[0];
+            // try to parse device types from settings
+            ComputeDeviceTypes deviceType;
+            if (!Enum.TryParse(Settings.Default.DeviceType, true, out deviceType))
+            {
+                // default to All device types
+                deviceType = ComputeDeviceTypes.All;
+            }
+            // get an OpenCL context and build all files within the given path
+            using (var context = new ComputeContext(deviceType, new ComputeContextPropertyList(platform), null, IntPtr.Zero))
+            {
+                BuildFiles(args[0], context);
+            }
+        }
+
+        private static void BuildFiles(string path, ComputeContext context)
+        {
             // iterate over OpenCL source files
-            foreach (var filename in Directory.EnumerateFiles(args[0], "*.cl", SearchOption.AllDirectories))
+            foreach (var filename in Directory.EnumerateFiles(path, "*.cl", SearchOption.AllDirectories))
             {
                 var program = new ComputeProgram(context, File.ReadAllText(filename));
                 try
@@ -70,9 +74,19 @@ Frontend phase failed compilation.
                     Console.WriteLine("OpenCL build failed: {0}", filename);
                     foreach (var device in context.Devices)
                     {
-                        //Console.WriteLine("Build result for {0}", device.Name);
-                        //Console.WriteLine("Status: {0}", program.GetBuildStatus(device));
-                        foreach (var message in ParseBuildLog(program.GetBuildLog(device), filename))
+                        var log = program.GetBuildLog(device);
+                        IEnumerable<string> messages;
+                        switch (context.Platform.Vendor)
+                        {
+                            default:
+                            case VendorIntel:
+                                messages = ParseBuildLogIntel(log, filename);
+                                break;
+                            case VendorAmd:
+                                messages = ParseBuildLogAmd(log, filename);
+                                break;
+                        }
+                        foreach (var message in messages)
                         {
                             Console.Out.WriteLine(message);
                         }
@@ -85,14 +99,25 @@ Frontend phase failed compilation.
             }
         }
 
+        private static IEnumerable<string> ParseBuildLogIntel(string log, string filename)
+        {
+            // filename(line,column): error #: message
+            foreach (var line in log.Split('\n'))
+            {
+                var match = ErrorRegexIntel.Match(line);
+                if (!match.Success) continue;
+                yield return string.Format("{0}({1},{2}): {3} 0: OpenCL error: {4}", filename, match.Groups[1].Value, match.Groups[2].Value,  match.Groups[3].Value, match.Groups[4].Value);
+            }
+        }
+
         /// <summary>
-        /// Parses the OpenCL build log to a format parsable by Visual Studio.
-        /// Most likely driver dependent. This works for the current driver from AMD (as of April 2015).
+        /// Parses the OpenCL build log generated by the AMD compiler to a format parsable by Visual Studio.
+        /// Tested on the current driver from AMD as of April 2015.
         /// </summary>
         /// <param name="log">The OpenCL build log.</param>
         /// <param name="filename">The filename of the file which was built.</param>
         /// <returns>An enumerable of error messages.</returns>
-        private static IEnumerable<string> ParseBuildLog(string log, string filename)
+        private static IEnumerable<string> ParseBuildLogAmd(string log, string filename)
         {
             var fileLine = true;
             Match match = null;
@@ -102,7 +127,7 @@ Frontend phase failed compilation.
                 // keep looking for a line that contains filename, line number and the first part of the error message
                 if (fileLine)
                 {
-                    match = ErrorRegex.Match(line);
+                    match = ErrorRegexAmd.Match(line);
                     if (!match.Success) continue;
                     fileLine = false;
                     message.Clear();
@@ -121,7 +146,7 @@ Frontend phase failed compilation.
                 // get the filename from the match
                 var errorFilename = match.Groups[1].Value;
                 // if it matches the random temporary filename given by the compiler replace it with the correct filename
-                errorFilename = TempFileRegex.Replace(errorFilename, filename);
+                errorFilename = TempFileRegexAmd.Replace(errorFilename, filename);
                 // now the actual error message can be assembled
                 // to have Visual Studio correctly parse the error message it has to be in this format:
                 // filename(line,column): error #: message
